dfb164b9ad695a59998b2f984a7e088b7e6ef400
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2021-10-16 17:53:33 +0530

    Java code execution for print statement in a class

---
+++ Java with Selenium/src/FirstJavaProgram.java
@@ -0,0 +1,11 @@
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Statement on printing");
+		System.out.println("syso + Ctrl + space = short cut to get println statement");
+
+	}
+
+}
