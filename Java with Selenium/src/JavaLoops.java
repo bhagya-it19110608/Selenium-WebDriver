a9ae19cc64fdb4c1a574927eaa01eabb3ac13eb3
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2021-10-17 15:47:45 +0530

    Loops in JAVA

---
+++ Java with Selenium/src/JavaLoops.java
@@ -0,0 +1,28 @@
+
+public class JavaLoops {
+
+	public static void main(String[] args) {
+		/*for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}*/
+		
+		/*int i = 0;
+		while (i<=10) {
+			System.out.println(i);
+		i++;
+		}
+		
+		while(true) {
+			System.out.println(i);
+		i++;
+		
+		}*/
+		
+		int j = 10;
+		
+		do {
+			System.out.println(j);
+		}while(j<0);
+	}
+
+}
