538ac9b0474f118f6c2cd639481d452f2dfecb8d
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2021-10-17 15:23:48 +0530

    Conditional statements in JAVA

--- Java with Selenium/src/SecondJavaClass.java
+++ Java with Selenium/src/SecondJavaClass.java
@@ -7,11 +7,57 @@ public class SecondJavaClass {
 		int a = 200;
 		int b = 300;
 		
-		System.out.println(str1 + " " + str2);
+		/*System.out.println(str1 + " " + str2);
 		System.out.println(a + b);
 		
 		System.out.println(a + b + str1 + str2);
-		System.out.println(str1 + str2 + a + b);
+		System.out.println(str1 + str2 + a + b);*/
+	
+		/*if(a>1000) {
+			System.out.println("A is greater than 1000");
+		}else {
+			System.out.println("A is less than 1000");
+		}*/
+	
+		/*if(a==100) {
+			System.out.println("A is 100");
+		} else if(a==200) {
+			System.out.println("A is 200");
+		} else if(a==300) {
+			System.out.println("A is 300");
+		}else {
+			System.out.println("Value doesn't present");
+		}*/
+		
+		int day = 3;
+		
+		switch(day) {
+		case 1:
+			System.out.println("Today is Monday");
+		break;
+		case 2:
+			System.out.println("Today is Tuesday");
+		break;
+		case 3:
+			System.out.println("Today is Wednesday");
+		break;
+		case 4:
+			System.out.println("Today is Thursday");
+		break;
+		case 5:
+			System.out.println("Today is Friday");
+		break;
+		case 6:
+			System.out.println("Heyyy!!!! It's weekend");
+		break;
+		case 7:
+			System.out.println("Heyyy!!!! It's weekend");
+		break;
+		default:
+			System.out.println("This not and existing day!!!!");
+		break;
+		
+		}
 	}
 
 }
