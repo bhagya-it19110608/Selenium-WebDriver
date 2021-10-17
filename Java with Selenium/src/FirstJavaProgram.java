2913672f1865f743d486907fbf073c71f39b03a5
bhagya-it19110608 <bhagyaweerasinghe02@gmail.com>
2021-10-17 13:29:49 +0530

    Identified Variables & DataTypes

--- Java with Selenium/src/FirstJavaProgram.java
+++ Java with Selenium/src/FirstJavaProgram.java
@@ -1,10 +1,30 @@
-
 public class FirstJavaProgram {
 
-	public static void main(String[] args) {
+	static int varSec = 98765;
+	
+	public static void firstMethod() {
+		int number = 12345;
 		
-		System.out.println("Statement on printing");
-		System.out.println("syso + Ctrl + space = short cut to get println statement");
+		boolean flag = true;
+		char character = 'A';
+		String city = "newyork";
+		long phonenumber = 987654321L;
+		double decimalvalue = 234.43D;
+		
+		System.out.println(number);
+		System.out.println(city);
+		System.out.println(varSec);
+		System.out.println(flag);
+		System.out.println(character);
+		System.out.println(phonenumber);
+		System.out.println(decimalvalue);
+	}
+	
+	
+	public static void main(String[] args) {
+		FirstJavaProgram.firstMethod();
+		System.out.println(varSec);
+				
 
 	}
 
